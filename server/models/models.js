caabc52bb8ba9f085c6fcd2147ed9ebbbbb433e8
Ismaila Bulcourt <ismaila.bulcourt@viacesi.fr>
2022-07-11 15:57:36 +0200

    adding auto-creation of database and collection

--- server/models/models.js
+++ server/models/models.js
@@ -5,7 +5,7 @@ bddConnect('rattrapageProjet')
 const TaskSchema = new mongoose.Schema({
     todo : String,
     isComplete : Boolean
-})
+},{autoCreate: true})
 
 const Task = mongoose.model('task',TaskSchema)
 
